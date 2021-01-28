6fc4eb4b8fa520344416be5682d3009713ff341b
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2021-01-28 14:22:08 -0500

    test github graphql api

---
+++ index.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch')
+
+require('dotenv').config();
+
+const ACCESS_TOKEN = process.env.MY_TOKEN
+
+const query = `
+    query {
+        repository(owner:"isaacs", name: "github") {
+            issues(states:CLOSED) {
+                totalCount
+            }
+        }
+    }`;
+
+// @ts-ignore
+fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    body: JSON.stringify({query}),
+    headers: {
+        'Authorization': `Bearer ${ACCESS_TOKEN}`
+    },
+}).then(res => res.text())
+  .then(body => console.log(body))
+  .catch(error => console.log(error)); 
