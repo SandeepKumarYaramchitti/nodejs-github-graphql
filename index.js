0b0edc0c21acae424ccbf31a0c9613733ae4106c
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2021-02-01 22:47:14 -0500

    refine github API query

--- index.js
+++ index.js
@@ -3,14 +3,49 @@ const fetch = require('node-fetch')
 require('dotenv').config();
 const ACCESS_TOKEN = process.env.MY_TOKEN
 
+
 const query = `
-    query {
-        repository(owner:"angular", name: "angular") {
-            issues(states:OPEN) {
-                totalCount
-            }
+  query {
+    repository(owner:"angular", name:"angular") {
+      issues(first:100, states:OPEN) {
+        totalCount
+        pageInfo {
+          hasNextPage
         }
-    }`;
+        edges {
+          node {
+            title
+            labels(first:5) {
+              edges {
+                node {
+                  name
+                }
+              }
+            }
+            comments(first:20) {
+              totalCount
+              edges {
+                node {
+                  author {
+                    avatarUrl
+                    login
+                  }
+                  createdAt
+                  reactions {
+                    totalCount
+                  }
+                }
+              }
+            }
+            reactions {
+              totalCount
+            } 
+          }
+        }
+      }
+    }
+  }
+  `    
 
 // @ts-ignore
 fetch('https://api.github.com/graphql', {
